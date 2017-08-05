5e80909baafa1885ee94b4407c23676c4de25f92
Frank Villasenor <frank.villasenor@gmail.com>
2017-08-05 16:47:19 -0500

    This is closer but it hangs and I don't believe it gets all of the log entries properly.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,70 +1,120 @@
 'use strict'
 
 const Promise = require('bluebird');
+const qqsort = require('qqsort');
 
 module.exports = (logSources, printer) => {
   
-  const logEntries = []; //-> array of promises
+  //Gather all Log Entries
+  gatherAllLogEntries(logSources)
+    .then((allEntries) => {
+      console.log('allEntries', allEntries);
+      
+      //Sort them.
+      return new Promise( (res, rej) => {
+        qqsort(allEntries, cmp3, (er) => { res(allEntries); });
+      }).then(( sortedEntries ) => {
+        console.log('qqsort promise then', sortedEntries);
+        sortedEntries.forEach( sortedEntry => {
+          printer.print(sortedEntry);
+        });
+      }); //I think this is right.
+    })
+    //I think print, once the rest of the code is working.
+    .then( (o) => { console.log('gatherAllLogEntries - final then', o) } )
+    .catch(er => console.error('gatherAllLogEntries - ', er));
   
-  //console.log(logSources);
-  console.log('=====async-sorted-merge============================================================');
-  let i = 0;
   
-  logSources.forEach( logSource => {
-  	// popAsync returns a promise and resolves to a single log entry.
-		//  We need to gather all of the log entries in the logSource
-		//  which means we need to call popAsync as long as there are more entries
-    //logEntries.push( logSource.popAsync() ); //This only gets the first though, right?
   
-    //logSource
-    //  .popAsync() //-> a promise with either false or logEntry
-			//We don't know if there is more until after it resolves.
-		
-		function handleResults(logEntry) {
-      const src = logSource;
-      if(logEntry) {
-        logEntries.push(logEntry);
-      	return src.popAsync().then(handleResults);
-      }
-		}
-		
-		logSource.popAsync().then( handleResults );
-		
-		
-		
-		// --------------
-		
-		function handleResult( logEntry ) {
-			if(!logEntry) {
-        logEntries.push(logEntry); //Insert
-        logSource.popAsync().then(handleResult);
-			}
-			//Otherwise done
-		}
-		
-		logSource
-			.popAsync()
-			.then( handleResult );
-		
-  	/*
-    let logEntry = logSource.pop(); //Returned in order
-    do {
-      //{ date: <date>, msg: <str> }
-      logEntries.push(logEntry);
-      i++;
-    } while( logEntry = logSource.pop() );
-    console.log(`${i} ----------------------------------------------------------`);
-    */
-  } );
-	
-  // Promise
-		// .all( logEntries )
-		// .then( entries => { //expect an array of resolved promises.
-  //     // entries[0]
-		// 		// .popAsync() //not a function.
-		// 		// .then(result1 => {
-		// 		// 	console.log(result1)
-		// 		// });
-		// 	console.log(entries)
-		// } );
 }
+
+// promise -> allLogEntries
+function gatherAllLogEntries(logSources){
+  const sourcePromises = [];
+  logSources.forEach( logSource1 => {
+    sourcePromises.push( outerGatherLogEntries(logSource1) );
+  });
+  const entries = [], currentSources = [];
+  return Promise.all(sourcePromises)
+    .then( (sourceRes) => {
+      let r = [];
+      sourceRes.forEach( (itm) => { r = r.concat(itm) } );
+      return r;
+    } )
+    .catch( (er) => console.error('gatherAllLogEntries error', er) );
+    
+    //
+    //.then( (result) => [] ); //Eventuall return the list, flattened.
+}
+
+function outerGatherLogEntries(logSource) {
+  const accumulatedList = [];
+  const result = gatherLogEntries(logSource);
+  return result;
+  
+  function gatherLogEntries(logSource) {
+    console.log('gatherLogEntries');
+    const addToList = (logEntry) => {
+      accumulatedList.push(logEntry);
+      gatherLogEntries(logSource); //Call recursivly.
+    };
+    return logSource
+      .popAsync()
+      .then( addToList )
+      .then(() => {
+        console.log('returning accumulatedList');
+        return accumulatedList;
+      })
+      .catch( er => { console.error('gatherLogEntries error', er) } );
+  }
+}
+
+
+
+function cmp3(l1,l2) {
+  const e1 = l1.date, e2 = l2.date;
+  return (e1 < e2) ? -1 : (e1 > e2) ? 1 : 0
+}
+
+/*
+module.exports = (logSources, printer) => {
+  
+  const logEntries = [];
+  const logEntryPromises = [];
+  
+  //logSources itself doesn't have anything special inside of it...
+  let i = 0;
+  function gatherLogEntries(logSource1) {
+    i++;
+    return logSource1
+      .popAsync()
+      .then((entry) => {
+        if(entry) {
+          logEntryPromises.push( gatherLogEntries(logSource1) );
+        }
+        return entry;
+      });
+  }
+  
+  
+  logSources.forEach(function(logSource) {
+    logEntryPromises.push( gatherLogEntries(logSource) );
+    //logSource.popAsync() -> returns a promise that resolves to a log entry or false.
+  });
+  
+  Promise
+    .all(logEntryPromises) //Will be lots.
+    .then( a => {
+      //a would be an array with all of the LogEntries
+      console.log(a.length, i);
+      console.log(a);
+    } );
+  
+}
+*/
+
+/*
+alternativly we can do some sort of accumlating alls where each promise ends in a then with the result and the item.
+.then( logEntry => { entry: logEntry, logSource: logSource } ) -> then on every all, if we're not all false, do the next "batch>
+Batch over and over like that until all resolve to false.
+ */
